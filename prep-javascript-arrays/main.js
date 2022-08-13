fab1bcbecc10b013a8f2c2c7fb652e618cc22a84
Tina Chia <tinachia_5@yahoo.com>
2022-08-13 04:47:38 +0000

    completed prep-javascript-arrays

---
+++ prep-javascript-arrays/main.js
@@ -0,0 +1,15 @@
+var languages = ['html', 'css', 'javascript', 'react', 'node', 'postgres'];
+console.log('langauge array', languages);
+
+var firstElement = languages[0];
+console.log('the first element of the array is', firstElement);
+
+var thirdElement = languages[2];
+console.log('the third element of the array is', thirdElement);
+
+var length = languages.length;
+console.log('the length of the array is', length);
+
+var lastIndex = languages.length - 1;
+var lastElement = languages[lastIndex];
+console.log('the last element of the array is', lastElement);
